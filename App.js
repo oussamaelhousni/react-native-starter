86b51528c2c97f63be849b1f72b9f2cd65f219aa
oussamaelhousni <oussamaelhousnioe@gmail.com>
2024-03-02 13:44:40 +0100

    works with Button and TextInput And View

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
+import { useState } from "react";
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi User</Text>
+      <TextInput
+        value={name}
+        placeholder={"Please enter your name"}
+        onChangeText={(val) => setName(val)}
+        style={styles.input}
+      />
+      <Text>{count}</Text>
+      <Button
+        color={"red"}
+        backgroundColor={"black"}
+        title="Increment"
+        onPress={() => {
+          setCount((prev) => prev + 1);
+          Alert.alert("pressed");
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    padding: 20,
+    display: "flex",
+    backgroundColor: "red",
+    color: "red",
+  },
+  input: {
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    maxWidth: "90%",
+    borderRadius: 70,
   },
 });
